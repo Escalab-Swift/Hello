69e47df7c5f84542ce98a5c6185b245a1ce1dac7
Mauricio Caro <mauricio.caro@me.com>
2020-12-18 18:19:08 -0300

    Initial Commit

---
+++ Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by Mauricio Caro on 18-12-20.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
